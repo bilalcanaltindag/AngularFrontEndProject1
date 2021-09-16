55b9e1f353824d94677840005a2ceb35da54296c
Bilal Can Altındağ <bilalcanaltindag@hotmail.com>
2021-09-17 00:51:48 +0300

    Added Bootstrap table and Edited product.component.ts

--- northwind/src/app/app.component.ts
+++ northwind/src/app/app.component.ts
@@ -8,10 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'northwind';
   user: string = 'Bilal Can Altındağ';
-  product = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
-  product1 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 15 }
-  product2 = { productId: 3, productName: 'Kitap', categoryId: 1, unitPrice: 12 }
-  product3 = { productId: 4, productName: 'Kulaklık', categoryId: 1, unitPrice: 16 }
-
-  products = [this.product, this.product1, this.product2, this.product3]
+  
 }
--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  product = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
+  product1 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 15 }
+  product2 = { productId: 3, productName: 'Kitap', categoryId: 1, unitPrice: 12 }
+  product3 = { productId: 4, productName: 'Kulaklık', categoryId: 1, unitPrice: 16 }
+
+  products = [this.product, this.product1, this.product2, this.product3];
+
   constructor() { }
 
   ngOnInit(): void {
