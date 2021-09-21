3147ec05b827ed4d50e6596721960b802205c3da
Bilal Can Altındağ <bilalcanaltindag@hotmail.com>
2021-09-21 19:48:55 +0300

    Product Updated

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,12 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
-  product1 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 15 }
-  product2 = { productId: 3, productName: 'Kitap', categoryId: 1, unitPrice: 12 }
-  product3 = { productId: 4, productName: 'Kulaklık', categoryId: 1, unitPrice: 16 }
-
-  products = [this.product, this.product1, this.product2, this.product3];
+  products: Product[] = [];
 
   constructor() { }
 
